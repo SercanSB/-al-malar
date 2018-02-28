701e6652b4b19ff2a5995484cfa2f07c788dcd3d
SercanSB <36644057+SercanSB@users.noreply.github.com>
2018-02-28 21:41:43 +0300

    Delete uygulama01.py

--- LICENSE.md/uygulama01.py
+++
@@ -1,9 +0,0 @@
-metin="Mevcudiyetinin ve istikbalinin yegâne temeli budur. Bu temel senin en kıymetli hazinendir. İstikbalde dahi seni bu hazineden mahrum etmek isteyecek, dahili ve harici bedhahların olacaktır."
-a=metin
-b=int(len(a)/2)
-print(a[:b])
-
-
-
-
-
